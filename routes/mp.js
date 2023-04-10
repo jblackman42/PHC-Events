04781becbf2f3383f58d97e0e606c6907211d391
Jblack262 <jblackman@pureheart.org>
2023-04-10 08:43:52 -0700

    update calendar

--- routes/mp.js
+++ routes/mp.js
@@ -49,4 +49,59 @@ router.get('/events', ensureAuthenticated, async (req, res) => {
   res.send(events)
 })
 
+router.get('/event-rooms', ensureAuthenticated, async (req, res) => {
+  const { eventIDs } = req.query;
+
+  const rooms = await axios({
+    method: 'get',
+    url: 'https://my.pureheart.org/ministryplatformapi/tables/Event_Rooms',
+    params: {
+      $filter: `Event_ID IN (${eventIDs.join()}) AND Cancelled=0`,
+      $select: `Event_Room_ID, Event_ID, Room_ID_Table.[Room_Name], Room_ID_Table_Building_ID_Table.Building_Name, Room_ID_Table_Building_ID_Table_Location_ID_Table.[Location_Name]`
+    },
+    headers: {
+      'content-type': 'application/json',
+      'authorization': `Bearer ${req.session.access_token}`
+    }
+  })
+    .then(response => response.data)
+    .catch(err => console.log(err))
+
+  res.send(rooms)
+})
+
+router.get('/places', ensureAuthenticated, async (req, res) => {
+  const places = await axios({
+    method: 'get',
+    url: 'https://my.pureheart.org/ministryplatformapi/tables/Rooms',
+    params: {
+      $filter: `Bookable=1 AND (Building_ID_Table.Date_Retired IS NULL OR Building_ID_Table.Date_Retired > GETDATE())`,
+      $select: 'Rooms.Room_ID, Rooms.Room_Name, Rooms.Building_ID, Building_ID_Table.[Building_Name], Building_ID_Table_Location_ID_Table.[Location_ID], Building_ID_Table_Location_ID_Table.Location_Name'
+    },
+    headers: {
+      'content-type': 'application/json',
+      'authorization': `Bearer ${req.session.access_token}`
+    }
+  })
+  .then(response => response.data)
+  .catch(err => console.log(err))
+
+  const locations = [...new Set(places.map(place => place.Location_Name))]
+  const locationsArr = locations.map(location => {
+    const buildings = [...new Set(places.filter(place => place.Location_Name == location).map(place => place.Building_Name))]
+    return {
+      Location_Name: location,
+      Buildings: buildings.map(building => {
+        const rooms = [...new Set(places.filter(place => place.Building_Name == building).map(place => place.Room_Name))]
+        return {
+          Building_Name: building,
+          Rooms: rooms
+        }
+      })
+    }
+  })
+
+  res.send(locationsArr)
+})
+
 module.exports = router;
--- views/scripts/calendar.js
+++ views/scripts/calendar.js
@@ -18,12 +18,23 @@ class PHC_Calendar extends HTMLElement {
   
   update = async () => {
     loading();
+    await this.getPlaces();
     await this.getEvents();
     this.createCalendar();
     doneLoading();
 
 
     console.log(this.events)
+  }
+
+  getPlaces = async () => {
+    this.places = await axios({
+      method: 'get',
+      url: '/api/mp/places'
+    })
+      .then(response => response.data)
+
+    console.log(this.places)
   }
 
   getEvents = async () => {
@@ -139,7 +150,6 @@ class PHC_Calendar extends HTMLElement {
     `
 
     const calendarDaysList = document.querySelectorAll('.calendar-day');
-    console.log(calendarDaysList)
     calendarDaysList.forEach((elem, i) => {
       elem.onclick = () => this.showDetailsPopup(this.monthArr[i])
     })
@@ -178,10 +188,22 @@ class PHC_Calendar extends HTMLElement {
     document.body.appendChild(eventDetailsPopupDOM)
   }
 
-  showDetailsPopup = (day) => {
+  showDetailsPopup = async (day) => {
+    this.hideDetailsPopup();
+    loading();
+
     const currDate = new Date(day);
     const endOfCurrDate = new Date(currDate.getTime() + 86399999)
     const daysEvents = this.events.filter(event => currDate <= new Date(event.Event_End_Date) && endOfCurrDate >= new Date(event.Event_Start_Date));
+
+    const daysEventsRooms = await axios({
+      method: 'get',
+      url: '/api/mp/event-rooms',
+      params: {
+        eventIDs: daysEvents.map(event => event.Event_ID)
+      }
+    })
+      .then(response => response.data)
 
     const eventDetailsPopupDOM = document.getElementById('popup-container');
     eventDetailsPopupDOM.classList.add('open');
@@ -192,26 +214,36 @@ class PHC_Calendar extends HTMLElement {
           <div class="title">
               <h1 id="event-date">${currDate.toLocaleDateString('en-us', { weekday:"short", year:"numeric", month:"short", day:"numeric"})}</h1>
               <p id="number-of-events">${daysEvents.length} Events</p>
-              <button class="close-button"><i class="material-icons">close</i></button>
+              <button class="close-button" id="details-close-btn"><i class="material-icons">close</i></button>
           </div>
           
           <ul id="events-list">
             ${daysEvents.map(event => {
               const { Event_ID, Event_Title, Minutes_for_Setup, Minutes_for_Cleanup, Location_Name, Event_End_Date, Event_Start_Date } = event;
+              const eventsRooms = daysEventsRooms.filter(room => room.Event_ID == Event_ID)
+
+              const eventStartTime = new Date(Event_Start_Date).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+              const eventEndTime = new Date(Event_End_Date).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+              const eventTimesString = `${eventStartTime} - ${eventEndTime}`;
+
+              const reservedStartTime = new Date(new Date(Event_Start_Date).getTime() - (Minutes_for_Setup * 60000)).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+              const reservedEndTime = new Date(new Date(Event_End_Date).getTime() + (Minutes_for_Cleanup * 60000)).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+              const reservedTimeString = `${reservedStartTime} - ${reservedEndTime}`;
+              
               return `
                 <li class="event" id="event-${Event_ID}">
                     <div style="flex-wrap:wrap;">
                         <h4 id="event-name">${Event_Title}</h4>
                     </div>
-                    ${Minutes_for_Setup && Minutes_for_Cleanup ? `
+                    ${Minutes_for_Setup || Minutes_for_Cleanup ? `
                         <div>
                             <p id="label">Reserved Time:</p>
-                            <p>${'reservedTimesString'}</p>
+                            <p>${reservedTimeString}</p>
                         </div>
                     ` : ''}
                     <div>
                         <p id="label">Event Time:</p>
-                        <p>${'eventTimesString'}</p>
+                        <p>${eventTimesString}</p>
                     </div>
                     <div>
                         <p>Location:</p>
@@ -219,7 +251,7 @@ class PHC_Calendar extends HTMLElement {
                     </div>
                     <div>
                         <p id="label">${'Rooms'}:</p>
-                        <p id="rooms-list">${'No Rooms Booked'}</p>
+                        <p id="rooms-list">${eventsRooms.length ? [...new Set(eventsRooms.map(room => room.Room_Name))].join(', ') : 'No Rooms Booked'}</p>
                     </div>
                     <div class="btn-container">
                         <a class="popup-btn" href="https://my.pureheart.org/mp/${308}/${Event_ID}" target="_blank"><i class="material-icons">description</i><span class="btn-label">View on MP</span></a>
@@ -233,6 +265,32 @@ class PHC_Calendar extends HTMLElement {
       </div>
       <button id="next-day-btn" onClick="nextDay()"><i class="material-icons">keyboard_arrow_right</i></button>
     `
+
+    const detailsCloseBtnDOM = document.getElementById('details-close-btn');
+    detailsCloseBtnDOM.onclick = this.hideDetailsPopup;
+
+    const nextDayBtn = document.getElementById('next-day-btn');
+    nextDayBtn.onclick = () => this.nextDay(day)
+    const prevDayBtn = document.getElementById('prev-day-btn');
+    prevDayBtn.onclick = () => this.prevDay(day)
+
+    doneLoading();
+  }
+
+  hideDetailsPopup = () => {
+    const eventDetailsPopupDOM = document.getElementById('popup-container');
+    eventDetailsPopupDOM.classList.remove('open');
+  }
+
+  nextDay = (day) => {
+    const nextDayIndex = this.monthArr.indexOf(day) + 1;
+    const nextDay = this.monthArr[nextDayIndex];
+    if (nextDay) this.showDetailsPopup(nextDay)
+  }
+  prevDay = (day) => {
+    const prevDayIndex = this.monthArr.indexOf(day) - 1;
+    const prevDay = this.monthArr[prevDayIndex];
+    if (prevDay) this.showDetailsPopup(prevDay)
   }
 }
 
