1a5a24b46d82f69180a4dc6e3ed617ab2217dd2d
jblackman42 <jblackman@pureheart.org>
2023-10-02 14:03:05 -0700

    re-added ticket route

--- routes/helpdesk.js
+++ routes/helpdesk.js
@@ -157,4 +157,37 @@ app.get('/toggle-polling', ensureApiAuthenticated, (req, res) => {
 // auto start polling
 pollingTimer = setInterval(checkForNewMessagesAndReply, pollingMs); // Poll every 20 seconds
 
+
+
+
+// old
+
+app.get('/tickets', async (req, res) => {
+    const accessToken = await axios({
+        method: 'post',
+        url: 'https://my.pureheart.org/ministryplatformapi/oauth/connect/token',
+        data: qs.stringify({
+            grant_type: "client_credentials",
+            scope: "http://www.thinkministry.com/dataplatform/scopes/all",
+            client_id: process.env.APP_CLIENT_ID,
+            client_secret: process.env.APP_CLIENT_SECRET
+        })
+    })
+        .then(response => response.data.access_token)
+        .catch(err => console.error(err))
+  
+    const data = await axios({
+        method: 'post',
+        url: `https://my.pureheart.org/ministryplatformapi/procs/api_Widget_HelpdeskTickets`,
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.data[0])
+        .catch(err => console.error(err))
+  
+    res.status(200).send(data).end();
+})
+
 module.exports = app;
